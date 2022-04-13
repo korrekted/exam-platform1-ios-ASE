196e643058a028f923e4586c3fe696ac4e85a055
akonst17 <akonst17@gmail.com>
2022-04-13 11:20:47 +0300

    upgrade; build 4

--- ASE/Classes/Service/Stats/Share/StatsShareManager.swift
+++ ASE/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ase.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ase.widget")
     }
     
     @discardableResult
