c49ec99304f6586ac736d5b3d891620eabd5bed3
akonst17 <akonst17@gmail.com>
2022-09-20 19:12:22 +0300

    интеграция sdk 2.0; новые урлы; build 9

--- ASE/Classes/Misc/GlobalDefinitions.swift
+++ ASE/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "ZOgh8gHuz7Gk9q8Y"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "x8dXJGiOcVb4CYd4"
 
     static let applicationTag = "EXAM_ASE"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "x8dXJGiOcVb4CYd4"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "XErDyzxA4yQu0hxr"
 
     static let appStoreUrl = "https://apps.apple.com/app/ase-automotive-test-exam-prep/id1583611129"
     static let appleAppID = "1583611129"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
